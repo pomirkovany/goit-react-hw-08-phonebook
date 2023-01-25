7f1f9e1ef9d77aa8cd407429885b7f3aaf748c63
Mrichko Bohdan <zaq4769@gmail.com>
2023-01-25 12:27:03 +0200

    update taskSlice

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -45,22 +45,7 @@ const contactsSlice = createSlice({
       state.isLoading = false;
       state.error = null;
     },
-
-    //   addContact: {
-    //     reducer(store, { payload }) {
-    //       store.push(payload);
-    //     },
-    //     prepare(data) {
-    //       const newContact = { ...data, id: nanoid() };
-    //       return {
-    //         payload: newContact,
-    //       };
-    //     },
-    //   },
-    //   deleteContact: (store, { payload }) =>
-    //     store.filter(({ id }) => id !== payload),
   },
 });
 
-// export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
